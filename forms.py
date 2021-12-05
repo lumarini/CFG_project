4d2b6f4dd1d81b3641d1cbe77301b6e50beb7e44
Saamiya Yousuf <65776360+Saamiya96@users.noreply.github.com>
2021-12-05 05:05:11 +0000

    Front-end
    Hey everyone, here is the basic skeleton of the front-end of our project. I tried to upload the folders seperatley, but my GitHub isn't in the best shape! Hopefully its clear to you where each file belongs to for each folder

---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=6, max=12)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max=12)])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password"), Length(min=6, max=12)])
+    submit = SubmitField("Sign Up!")
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=6, max=12)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max=12)])
+    remember_me = BooleanField("Remember Me!")
+    submit = SubmitField("Login")
+
+class Input_User(FlaskForm):
+    genre = StringField("What genre of film are you interested in?", validators=[DataRequired()])
+    run_time = StringField("How long is the film?", validators=[DataRequired()])
+    rating = StringField("What age rating do you prefer?", validators=[DataRequired()])
+    submit = SubmitField("Enter your choices!")
+
